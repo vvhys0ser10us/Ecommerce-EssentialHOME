870cfe07a2709476c3b523323f99c6e2fabdf927
kens0serious <kens0serious@gmail.com>
2023-02-12 03:25:41 -0500

    Add error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Error = () => {
-  return <div>Error</div>
+  return (
+    <Wrapper className="page-100">
+      <section>
+        <h1>404</h1>
+        <h3>Page Not Found</h3>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+      </section>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.main`
+  background: var(--clr-primary-10);
+  display: grid;
+  place-content: center;
+  text-align: center;
+  section {
+    margin-bottom: 10rem;
+  }
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    margin-bottom: 3rem;
+  }
+`
 
 export default Error
