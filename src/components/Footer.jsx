63a8f583e4a56beaa51642936e3bb844768539c3
kens0serious <kens0serious@gmail.com>
2023-02-12 00:36:39 -0500

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Footer = () => {
-  return <div>Footer</div>
+  const year = new Date().getFullYear()
+
+  return (
+    <Wrapper>
+      <p>
+        &copy; {year} <span>E-commerce</span> all rights reserved
+      </p>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.footer`
+  background: var(--clr-black);
+  display: grid;
+  place-items: center;
+  p {
+    color: var(--clr-white);
+    text-transform: capitalize;
+    letter-spacing: var(--spacing);
+    margin: 2rem auto;
+  }
+  span {
+    color: var(--clr-primary-5);
+  }
+`
 
 export default Footer
