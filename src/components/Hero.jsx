7f84c67a5ba81df001c158248f41ca72246446bf
kens0serious <kens0serious@gmail.com>
2023-02-13 00:35:27 -0500

    Complete home page hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,103 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import herobcg1 from '../assets/hero-bcg.jpeg'
+import herobcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
-  return <div>Hero</div>
+  return (
+    <Wrapper className="section-center">
+      <article>
+        <h1>
+          design your
+          <br /> comfort zone
+        </h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, at
+          sed omnis corporis doloremque possimus velit! Repudiandae nisi odit,
+          aperiam odio ducimus, obcaecati libero et quia tempora excepturi quis
+          alias?
+        </p>
+        <Link to="products" className="btn hero-btn">
+          shop now
+        </Link>
+      </article>
+
+      <article className="hero-imgs">
+        <img className="hero-1" src={herobcg1} alt="hero img" />
+        <img className="hero-2" src={herobcg2} alt="hero img" />
+      </article>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.section`
+  min-height: 60vh;
+  display: grid;
+  place-items: center;
+
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin-bottom: 2rem;
+    color: var(--clr-grey-5);
+    font-size: 1rem;
+  }
+
+  .hero-imgs {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    height: calc(100vh - 5rem);
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 8rem;
+
+    h1 {
+      margin-bottom: 2rem;
+    }
+    p {
+      font-size: 1.25rem;
+    }
+
+    .hero-btn {
+      padding: 0.75rem 1.5rem;
+      font-size: 1rem;
+    }
+
+    .hero-imgs {
+      display: block;
+      position: relative;
+      &::before {
+        content: '';
+        position: absolute;
+        width: 10%;
+        height: 80%;
+        background: var(--clr-primary-9);
+        bottom: 0%;
+        left: -8%;
+        border-radius: var(--radius);
+      }
+    }
+
+    .hero-1 {
+      width: 100%;
+      height: 550px;
+      border-radius: var(--radius);
+      display: block;
+      object-fit: cover;
+      position: relative;
+    }
+
+    .hero-2 {
+      width: 250px;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      transform: translateX(-50%);
+    }
+  }
+`
 
 export default Hero
