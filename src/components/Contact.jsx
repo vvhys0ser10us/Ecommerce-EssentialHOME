b6493b4c82f933a9ea5936c8baf0c6e2781a80d4
kens0serious <kens0serious@gmail.com>
2023-02-13 01:13:00 -0500

    Adjust padding at bigger screen

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import styled from 'styled-components'
 const Contact = () => {
   return (
-    <Wrapper>
+    <Wrapper className="section">
       <div className="section-center">
         <h3>Join our newsletter and get 20% off</h3>
         <div className="content">
@@ -28,7 +28,6 @@ const Contact = () => {
   )
 }
 const Wrapper = styled.section`
-  padding: 5rem 0;
   h3 {
     text-transform: none;
   }
@@ -87,7 +86,7 @@ const Wrapper = styled.section`
     }
   }
   @media (min-width: 1280px) {
-    padding: 15rem 0;
+    padding-top: 15rem;
   }
 `
 
