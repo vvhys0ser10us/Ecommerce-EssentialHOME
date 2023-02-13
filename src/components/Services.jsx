e0e2fafe6508a31b37d9f28be743a3173a9f1689
kens0serious <kens0serious@gmail.com>
2023-02-13 01:11:17 -0500

    Adjust position  of service cards at bigger screen

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -96,6 +96,13 @@ const Wrapper = styled.section`
       grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
     }
   }
+  @media (min-width: 1280px) {
+    padding: 0;
+
+    .section-center {
+      transform: translateY(5rem);
+    }
+  }
 `
 
 export default Services
