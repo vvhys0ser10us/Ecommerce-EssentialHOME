2599ad2a1dc3f9bf7056011d64775723e32b7474
kens0serious <kens0serious@gmail.com>
2023-02-12 23:40:42 -0500

    Add home page components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,5 +2,18 @@ import Navbar from './Navbar'
 import Footer from './Footer'
 import Sidebar from './Sidebar'
 import PageHero from './PageHero'
+import Contact from './Contact'
+import FeaturedProducts from './FeaturedProducts'
+import Hero from './Hero'
+import Services from './Services'
 
-export { Navbar, Footer, Sidebar, PageHero }
+export {
+  Navbar,
+  Footer,
+  Sidebar,
+  PageHero,
+  Contact,
+  FeaturedProducts,
+  Hero,
+  Services,
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { FeaturedProducts, Services, Contact, Hero } from '../components'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <main>
+      <Hero />
+      <FeaturedProducts />
+      <Services />
+      <Contact />
+    </main>
+  )
 }
 
 export default Home
