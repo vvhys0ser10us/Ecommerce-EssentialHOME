b86d1d4e5f663a4449a43d15f8ab816eab9dad81
kens0serious <kens0serious@gmail.com>
2023-02-14 04:08:39 -0500

    feat: SingleProduct page(in progress)

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useProductsContext } from '../context/products_context'
+import { PageHero } from '../components'
+import { Loading, Error, ProductImages } from '../components'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const SingleProduct = () => {
-  return <div>SingleProduct</div>
+  const { id } = useParams()
+  const {
+    s_isLoading: loading,
+    s_error: error,
+    singleProduct,
+    fetchSingleProduct,
+  } = useProductsContext()
+
+  useEffect(() => {
+    fetchSingleProduct(id)
+  }, [id])
+
+  if (loading) {
+    return <Loading></Loading>
+  }
+
+  if (error) {
+    return <Error></Error>
+  }
+
+  const { name, images } = singleProduct
+
+  return (
+    <>
+      <PageHero title={name} product={true}></PageHero>
+      <Wrapper className="section section-center">
+        <Link className="btn" to="/products">
+          back to products
+        </Link>
+
+        <article>
+          <ProductImages images={images}></ProductImages>
+
+          <div className="product-info">info</div>
+        </article>
+      </Wrapper>
+    </>
+  )
 }
+
+const Wrapper = styled.section``
 
 export default SingleProduct
