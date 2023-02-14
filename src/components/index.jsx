14d992ac96616be239095b6e8eaa464b7c5f661e
kens0serious <kens0serious@gmail.com>
2023-02-14 02:29:52 -0500

    Feat: error component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -7,6 +7,7 @@ import FeaturedProducts from './FeaturedProducts'
 import Hero from './Hero'
 import Services from './Services'
 import Loading from './Loading'
+import Error from './Error'
 
 export {
   Navbar,
@@ -18,4 +19,5 @@ export {
   Hero,
   Services,
   Loading,
+  Error,
 }
