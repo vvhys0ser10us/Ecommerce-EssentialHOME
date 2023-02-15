f0564a0ba9679aa48bb35df3a4f42fd9d8fd7c25
kens0serious <kens0serious@gmail.com>
2023-02-14 23:31:33 -0500

    Feat: add product info section

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -5,6 +5,7 @@ import { PageHero } from '../components'
 import { Loading, Error, ProductImages } from '../components'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import { formatPrice } from '../utils/helpers'
 
 const SingleProduct = () => {
   const { id } = useParams()
@@ -27,26 +28,89 @@ const SingleProduct = () => {
     return <Error></Error>
   }
 
-  const { name, images } = singleProduct
+  const {
+    name,
+    images,
+    price,
+    description,
+    stock,
+    stars,
+    review,
+    shipping,
+    company,
+  } = singleProduct
 
   return (
     <>
       <PageHero title={name} product={true}></PageHero>
-      <Wrapper className="section section-center">
+      <Wrapper className="section section-center page">
         <Link className="btn" to="/products">
           back to products
         </Link>
 
-        <article>
+        <section className="main">
           <ProductImages images={images}></ProductImages>
-
-          <div className="product-info">info</div>
-        </article>
+          <article className="content">
+            <div className="product-info">
+              <h2>{name}</h2>
+              <div>stars</div>
+              <h5 className="price">{formatPrice(price)}</h5>
+              <p className="description">{description}</p>
+              <p className="info">
+                <span>Available :</span>{' '}
+                {stock > 0 ? 'in stock' : 'out of stock'}
+              </p>
+              <p className="info">
+                <span>SKU :</span> {id}
+              </p>
+              <p className="info">
+                <span>Brand :</span> {company}
+              </p>
+              <hr />
+              {stock > 0 && <div>Add to cart</div>}
+            </div>
+          </article>
+        </section>
       </Wrapper>
     </>
   )
 }
 
-const Wrapper = styled.section``
+const Wrapper = styled.section`
+  .main {
+    display: grid;
+    gap: 4rem;
+    margin-top: 2rem;
+  }
+  .price {
+    color: var(--clr-primary-5);
+  }
+
+  .description {
+    max-width: 45rem;
+    line-height: 2rem;
+  }
+
+  .info {
+    text-transform: capitalize;
+    width: 300px;
+    display: grid;
+    grid-template-columns: 125px 1fr;
+    span {
+      font-weight: 700;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .main {
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+    }
+
+    .price {
+      font-size: 1.25rem;
+    }
+  }
+`
 
 export default SingleProduct
