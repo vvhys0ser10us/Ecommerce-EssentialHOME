2ca684e1d810424dfb3de21a2e59902879583ba9
kens0serious <kens0serious@gmail.com>
2023-02-15 16:16:35 -0500

    Products page layout

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
+import { PageHero, ProductList } from '../components'
 
 const Products = () => {
-  return <div>Products</div>
+  return (
+    <main>
+      <PageHero title="products"></PageHero>
+      <Wrapper className="page">
+        <div className="section-center products">
+          <div>Filter</div>
+          <div>
+            <div>sort</div>
+            <ProductList></ProductList>
+          </div>
+        </div>
+      </Wrapper>
+    </main>
+  )
 }
 
+const Wrapper = styled.div`
+  .products {
+    display: grid;
+    gap: 3rem 1.5rem;
+    margin: 4rem auto;
+  }
+
+  @media (min-width: 768px) {
+    .products {
+      grid-template-columns: 200px 1fr;
+    }
+  }
+`
 export default Products
