69c40d73938392b4e730eb949830ef2de78dbf61
kens0serious <kens0serious@gmail.com>
2023-02-17 02:52:38 -0500

    Feat: local storage for cart items

--- src/context/cart_context.jsx
+++ src/context/cart_context.jsx
@@ -1,12 +1,22 @@
-import { useContext, useReducer } from 'react'
+import { useContext, useReducer, useEffect } from 'react'
 import { createContext } from 'react'
 import cart_reducer from '../reducers/cart_reducer'
 import { ADD_TO_CART } from '../actions'
 
 const CartContext = createContext()
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem('cart')
+
+  if (cart) {
+    return JSON.parse(cart)
+  } else {
+    return []
+  }
+}
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   item_amount: 0,
   shipping: 1500,
@@ -16,12 +26,24 @@ const initialState = {
 const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cart_reducer, initialState)
 
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(state.cart))
+  }, [state.cart])
+
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } })
   }
 
+  const removeItem = () => {}
+
+  const toggleAmount = (id, value) => {}
+
+  const clearCart = () => {}
+
   return (
-    <CartContext.Provider value={{ ...state, addToCart }}>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
+    >
       {children}
     </CartContext.Provider>
   )
