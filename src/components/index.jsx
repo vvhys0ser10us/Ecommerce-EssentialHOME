870b2662728b1e18f3258d737ca3e30d198a216b
kens0serious <kens0serious@gmail.com>
2023-02-17 02:57:37 -0500

    Add cart components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -14,6 +14,7 @@ import AddToCart from './AddToCart'
 import ProductList from './ProductList'
 import Filters from './Filters'
 import Sort from './Sort'
+import CartContent from './CartContent'
 
 export {
   Navbar,
@@ -32,4 +33,5 @@ export {
   ProductList,
   Filters,
   Sort,
+  CartContent,
 }
